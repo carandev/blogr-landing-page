4c671a0953cb3b148ed3d1a7a8a7ebfdf871e19b
Carlos Gomez <carlosandres0741@gmail.com>
2022-01-18 21:11:08 -0500

    Feature: functionality in menu

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const $hamburgerIcon = document.querySelector(".hamburger-icon")
 const $header__nav = document.querySelector(".header__nav")
-const $itemNavConnect = document.querySelector(".connect")
+const $itemsNav = document.querySelectorAll(".item-nav")
 const $connectContainer = document.querySelector(".header__connect-container")
 
 $hamburgerIcon.addEventListener("click", ()  => {
   $header__nav.classList.toggle("hide")
+  $hamburgerIcon.classList.toggle("change-icon")
 })
 
-$itemNavConnect.addEventListener("click", () => {
-  $connectContainer.classList.toggle("hide")
-})
+const classToFlip = element => {
+  const containerToShow = document.querySelector(`#${element.classList[1]}`)
+  if (containerToShow) {
+    containerToShow.classList.toggle("hide")
+    element.classList.toggle("flip")
+  } 
+}
+
+$itemsNav.forEach(element => element.addEventListener('click', () => classToFlip(element)))
